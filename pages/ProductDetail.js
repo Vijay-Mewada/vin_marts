e6a372a291e71f0231895c53d4f47e1a37bcf0ee
vijay-mewada <vijaymewada2012@gmail.com>
2022-03-13 21:14:59 +0530

    category updated

--- pages/ProductDetail.js
+++ pages/ProductDetail.js
@@ -3,22 +3,25 @@ import ReactImageMagnify from 'react-image-magnify';
 
 const ProductDetail = () => {
   return (
-    <div>
+    <div className='flex'>
+      <img src='/images/strach.png' className='w-12 h-12 border-2 m-2 border-black'/>
         <ReactImageMagnify {...{
     smallImage: {
         alt: 'Wristwatch by Ted Baker London',
-        width: 256,
-        height:192,
+        width: 666,
+        height:500,
         // isFluidWidth: true,
-        src: "/images/strach.png",
+        src: "/images/aloverajuice.webp",
         sizes: '(min-width: 800px) 33.5vw, (min-width: 415px) 50vw, 100vw',
     },
     largeImage: {
-        src: "/images/strach.png",
-        width: 1200,
+        src: "/images/aloverajuice.webp",
+        width: 800,
         height: 1800
     }
 }} />
+
+
     </div>
   )
 }
